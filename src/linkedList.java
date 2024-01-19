5dc5fd9e7cf4fd80977d1e9faf5262aac5c6bb95
ankitkumar1302 <ankit1302@outlook.com>
2024-01-19 13:10:35 +0530

    linkedList

---
+++ src/linkedList.java
@@ -0,0 +1,11 @@
+public class linkedList {
+    /*
+    Musical Playlist
+    You are developing s music streaming application, and you want to implement a playlist feature each node in the
+    linked list representation a song and the linked list maintain the order of song in the playlist
+     */
+
+
+
+
+}
