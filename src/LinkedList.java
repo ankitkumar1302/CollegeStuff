947ef63939587cb30074984c88478e276e94687e
ankitkumar1302 <ankit1302@outlook.com>
2024-01-28 22:54:11 +0530

    LinkedList updated

---
+++ src/LinkedList.java
@@ -0,0 +1,29 @@
+
+
+//public class linkedList {
+//    /*
+//    Musical Playlist
+//    You are developing s music streaming application, and you want to implement a playlist feature each node in the
+//    linked list representation a song and the linked list maintain the order of song in the playlist
+//     */
+//
+//    ///// search concepts in LinkedList /////
+//    public boolean contains(int data){
+//
+//
+//        return false;
+//    }
+//
+//}
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    public Node getHead(){
+        return this.head;
+    }
+
+    public Node getTail(){
+        return this.tail;
+    }
+}
--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,26 @@
 
-public class Node {
-    String data;
-    Node next;
-}
+//public class Node {
+//    String data;
+//    Node next;
+//}
 
+public class Node {
+    private String data;
+    private Node next;
+    public Node(String data){
+        this.data=data;
+    }
+    public void setData(String data){
+        this.data = data;
+    }
+    public void setNext(Node node){
+        this.next = node;
+    }
+    public String getData(){
+        return this.data;
+    }
+    public Node getNext(){
+        return this.next;
+    }
+}
 
